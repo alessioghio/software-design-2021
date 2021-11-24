9d1e8abf31fa92577c7744ccceadca26545ef4b0
raul.escandon <elmer.escandon@utec.edu.pe>
2021-11-23 22:28:25 -0500

    Revert "La cagué fuerte"
    This reverts commit aed505dbf3ff2d5d9db52599019957ef44512b72.

--- static/js/map.js
+++ static/js/map.js
@@ -4,7 +4,7 @@
 
 
 // When the window has finished loading create our google map below
-// google.maps.event.addDomListener(window, 'load', init);
+google.maps.event.addDomListener(window, 'load', init);
 
 function init() {
     // Basic options for a simple Google Map
